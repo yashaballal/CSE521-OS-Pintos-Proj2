db535aa70cb9f70e4e0aa4ca00b7a2097faffa00
Linus Castelino <castelinolinus@gmail.com>
2018-11-26 11:03:31 -0500

    testing 1

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -17,6 +17,13 @@
 #include "threads/palloc.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#include "threads/malloc.h"
+
+ #define WORD_SIZE sizeof(void *)
+ struct args_passed {
+  int argc;
+  char *argv[128];
+};
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
@@ -37,6 +44,11 @@ process_execute (const char *file_name)
   if (fn_copy == NULL)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
+
+  char file_name_copy[16];
+  char *save_ptr;
+  memcpy(file_name_copy, file_name, sizeof(file_name_copy));
+  const char *command = strtok_r(file_name_copy, " ", &save_ptr);
 
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
