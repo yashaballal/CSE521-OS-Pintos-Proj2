3032bb1026cccbe0650bda84eeb4ad333eb211ff
Linus Castelino <castelinolinus@gmail.com>
2018-11-26 18:51:44 -0500

    Added a skeleton for syscall switch case

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -15,6 +15,57 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+	int* stack_pointer = f->esp;
+	int syscall_num = *stack_pointer;
+
+	switch(syscall_num){
+		case SYS_HALT:
+			break;
+
+		case SYS_EXIT:
+			break;
+
+		case SYS_EXEC:
+			break;
+
+		case SYS_WAIT:
+			break;
+
+		case SYS_FORK:
+			break;
+
+		case SYS_CREATE:
+			break;
+
+		case SYS_REMOVE:
+			break;
+
+		case SYS_OPEN:
+			break;
+
+		case SYS_FILESIZE:
+			break;
+
+		case SYS_READ:
+			break;
+
+		case SYS_WRITE:
+			break;
+
+		case SYS_TELL:
+			break;
+
+		case SYS_SEEK:
+			break;
+
+		case SYS_TELL:
+			break;
+
+		case SYS_CLOSE:
+			break;
+
+	}
+
+  // printf ("system call!\n");
+  // thread_exit ();
 }
