cf3def362dde1b9325a683ca07717dcea2072e14
yashaballal <yashaballal@gmail.com>
2018-11-28 13:28:45 -0500

    some more changes

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -68,7 +68,6 @@ syscall_handler (struct intr_frame *f UNUSED)
 
 		case SYS_OPEN:
 		{
-            struct file_descriptor *fdesc = (struct file_descriptor *) malloc(sizeof(struct file_descriptor));
 			char *file_name = *((char **) args_refs[0]);
   			/*if(file_name == NULL)
     		        {
@@ -90,13 +89,15 @@ syscall_handler (struct intr_frame *f UNUSED)
   			  	 break;
   			  }
 
+  			  struct file_descriptor *fdesc = (struct file_descriptor *) malloc(sizeof(struct file_descriptor));
   			  /*Need to maintain a list of the files opened by the thread*/	
+  			  struct thread *thread_cur = thread_current();
   			  fdesc->fd = thread_current()->fd_counter;
-  			  thread_current()->fd_counter++;
+  			  thread_cur->fd_counter++;
   			  fdesc->file = file_n;
-  			  printf("Came till here");
-  			  list_push_back(&thread_current()->fd_list, &fdesc->fd_elem);
-  			  printf("The next line" );
+  			  //printf("Came till here");
+  			  list_push_back(&thread_curr->fd_list, &fdesc->fd_elem);
+  			  //printf("The next line" );
   			  f->eax = fdesc->fd;
   			break;
         }
