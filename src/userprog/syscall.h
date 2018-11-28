ef111699b590b8a74fb63f58180fb2144ebd6ed8
Linus Castelino <castelinolinus@gmail.com>
2018-11-28 12:07:06 -0500

    Correcting build error

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -10,14 +10,14 @@ struct file_descriptor{
     struct list_elem fd_elem;    	// list_elem to use as a reference in a list
     struct file *file;    			// pointer to file referenced by the current file descriptor
     struct fd_buf *fd_buf;    		// fd buffer for piped file I/O 
-}
+};
 
 struct fd_buf{
     int rw_count;    				// count to check how many streams are open (0, 1, 2)
     char fd_buffer[MAX_BUF_SIZE];  	// buffer used to exchange messages in piped mode
     struct lock fd_buffer_lock;    	// lock to synchronize the fd_buffer
     int buf_start, buf_end;    		// start and end indexes to fd_buffer array
-}
+};
 
 
 #endif /* userprog/syscall.h */
